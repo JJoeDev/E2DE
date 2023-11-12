33e53267145a0d4c7c5d477fc63390d25cc5ff5e
JJoe <48448023+JJoeDev@users.noreply.github.com>
2023-11-12 21:16:51 +0100

    Getting ready to use EnTT
    Added a comment as to why we use the real path to entt.hpp file

--- E2DE/src/Scene/Scene.h
+++ E2DE/src/Scene/Scene.h
@@ -1,7 +1,7 @@
 #ifndef E2E_SCENE_H_
 #define E2E_SCENE_H_
 
-#include "../../Vendor/ECS/EnTT/src/entt/entt.hpp"
+#include "../../Vendor/ECS/EnTT/src/entt/entt.hpp" // We can just say #include "entt.hpp" but vscodium can't find the header so I do this for now
 
 namespace e2e{
     class Scene{
