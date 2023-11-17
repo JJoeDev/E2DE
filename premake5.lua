69b59c4f79b35b4df96cda3be90c2c98e6f17437
jjoedev <jhknuds1@protonmail.com>
2023-11-17 17:54:26 +0100

    Trying some more....

--- premake5.lua
+++ premake5.lua
@@ -24,17 +24,9 @@ workspace "E2DE"
         }
         optimize "On"
 
-function setBxCompact()
-    filter { "system:linux" }
-        includedirs { path.join(BX_DIR, "include/compact/linux") }
-end
-
 project "E2DE"
     kind "StaticLib"
     language "C++"
-    cppdialect "C++17"
-    exceptionhandling "Off"
-    rtti "Off"
     targetdir ("bin/%{cfg.buildcfg}")
     objdir ("obj/%{cfg.buildcfg}")
 
@@ -49,8 +41,8 @@ project "E2DE"
             path.join(SDL_DIR, "build/include/SDL2"),
             path.join(ENTT_DIR, "src/entt"),
             path.join(BGFX_DIR, "include"),
-            path.join(BX_DIR, "include"),
-            path.join(BIMG_DIR, "include"),
+            --path.join(BX_DIR, "include"),
+            --path.join(BIMG_DIR, "include"),
         }
 
     filter "system:linux"
@@ -60,95 +52,13 @@ project "E2DE"
             "E2DE/Vendor/BGFX/bgfx/.build/linux64_gcc/bin",
         }
 
+    filter "system:linux"
+        links { "GL", "pthread", "X11", "rt" }
+
     links{
-        "bgfx",
-        "bimg",
-        "bx",
+        "bgfxRelease",
+        --"bimg",
+        --"bx",
         "SDL2",
         "SDL2main",
     }
-
-    filter "system:linux"
-        links { "dl", "GL", "pthread", "X11" }
-    setBxCompact()
-
-project "bgfx"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++14"
-    exceptionhandling "Off"
-    rtti "Off"
-    defines "__STDC_FORMAT_MACROS"
-
-    files{
-        path.join(BGFX_DIR, "include/bgfx/**.h"),
-        path.join(BGFX_DIR, "src/*.cpp"),
-        path.join(BGFX_DIR, "src/*.h"),
-    }
-
-    excludes{
-        path.join(BGFX_DIR, "src/amalgamated.cpp"),
-        path.join(BGFX_DIR, "src/dxgi.cpp"),
-        path.join(BGFX_DIR, "src/renderer_d3d11.cpp"),
-        path.join(BGFX_DIR, "src/renderer_d3d12.cpp"),
-    }
-
-    includedirs{
-        path.join(BX_DIR, "include"),
-        path.join(BIMG_DIR, "include"),
-        path.join(BGFX_DIR, "include"),
-        path.join(BGFX_DIR, "3rdparty"),
-        path.join(BGFX_DIR, "3rdparty/dxsdk/include"),
-        path.join(BGFX_DIR, "3rdparty/khronos"),
-    }
-
-    setBxCompact()
-
-project "bimg"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++14"
-    exceptionhandling "Off"
-    rtti "Off"
-
-    files{
-        path.join(BIMG_DIR, "include/bimg/*.h"),
-        path.join(BIMG_DIR, "src/image.cpp"),
-        path.join(BIMG_DIR, "src/image_gnf.cpp"),
-        path.join(BIMG_DIR, "src/*.h"),
-        path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc"),
-    }
-
-    includedirs{
-        path.join(BX_DIR, "include"),
-        path.join(BIMG_DIR, "include"),
-        path.join(BIMG_DIR, "3rdparty/astc-codec"),
-        path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
-    }
-    setBxCompact()
-
-project "bx"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++14"
-    exceptionhandling "Off"
-    rtti "Off"
-    defines "__STDC_FORMAT_MACROS"
-
-    files{
-        path.join(BX_DIR, "include/bx/*.h"),
-        path.join(BX_DIR, "include/bx/inline/*.inl"),
-        path.join(BX_DIR, "src/*.cpp"),
-    }
-
-    excludes{
-        path.join(BX_DIR, "src/amalgamated.cpp"),
-        path.join(BX_DIR, "src/crtnone.cpp"),
-    }
-
-    includedirs{
-        path.join(BX_DIR, "3rdparty"),
-        path.join(BX_DIR, "include"),
-    }
-    
-    setBxCompact()
