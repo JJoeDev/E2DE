6f4296f7d07780279b0d15db6feb1c0a23999e21
jjoedev <jhknuds1@protonmail.com>
2023-11-17 15:48:11 +0100

    Working on premake. why is bgfx so hard?

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,40 @@
+local BGFX_DIR  = "E2DE/Vendor/BGFX/bgfx"
+local BIMG_DIR  = "E2DE/Vendor/BGFX/bimg"
+local BX_DIR    = "E2DE/Vendor/BGFX/bx"
+local SDL_DIR   = "E2DE/Vendor/SDL/SDL_CORE/"
+local ENTT_DIR  = "E2DE/Vendor/ECS/EnTT"
+
 workspace "E2DE"
     configurations { "Debug", "Release" }
     architecture "x64"
     location "build"
 
     filter "configurations:Debug"
-        defines { "DEBUG" }
+        defines { 
+            "DEBUG",
+            "BX_CONFIG_DEBUG=1"
+        }
+        optimize "Debug"
         symbols "On"
 
     filter "configurations:Release"
-        defines { "NDEBUG" }
+        defines { 
+            "NDEBUG",
+            "BX_CONFIG_DEBUG=0",
+        }
         optimize "On"
+
+function setBxCompact()
+    filter { "system:linux" }
+        includedirs { path.join(BX_DIR, "include/compact/linux") }
+end
 
 project "E2DE"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    exceptionhandling "Off"
+    rtti "Off"
     targetdir ("bin/%{cfg.buildcfg}")
     objdir ("obj/%{cfg.buildcfg}")
 
@@ -25,11 +46,11 @@ project "E2DE"
     filter "system:linux" -- ADD WINDOWS SUPPORT LATER
         includedirs{
             "E2DE/src",        
-            "E2DE/Vendor/SDL/build/include/SDL2",
-            "E2DE/Vendor/ECS/EnTT/src/entt",
-            "E2DE/Vendor/BGFX/bgfx/include",
-            "E2DE/Vendor/BGFX/bx/include",
-            "E2DE/Vendor/BGFX/bimg/include"
+            path.join(SDL_DIR, "build/include/SDL2"),
+            path.join(ENTT_DIR, "src/entt"),
+            path.join(BGFX_DIR, "include"),
+            path.join(BX_DIR, "include"),
+            path.join(BIMG_DIR, "include"),
         }
 
     filter "system:linux"
@@ -39,14 +60,95 @@ project "E2DE"
             "E2DE/Vendor/BGFX/bgfx/.build/linux64_gcc/bin",
         }
 
+    links{
+        "bgfx",
+        "bimg",
+        "bx",
+        "SDL2",
+        "SDL2main",
+    }
+
     filter "system:linux"
-        links{
-            "SDL2",
-            "SDL2main",
-            "bgfx-shared-libRelease",
-            "GL",
-            "X11",
-            "dl",
-            "pthread",
-            "rt",
-        }
+        links { "dl", "GL", "pthread", "X11" }
+    setBxCompact()
+
+project "bgfx"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++14"
+    exceptionhandling "Off"
+    rtti "Off"
+    defines "__STDC_FORMAT_MACROS"
+
+    files{
+        path.join(BGFX_DIR, "include/bgfx/**.h"),
+        path.join(BGFX_DIR, "src/*.cpp"),
+        path.join(BGFX_DIR, "src/*.h"),
+    }
+
+    excludes{
+        path.join(BGFX_DIR, "src/amalgamated.cpp"),
+        path.join(BGFX_DIR, "src/dxgi.cpp"),
+        path.join(BGFX_DIR, "src/renderer_d3d11.cpp"),
+        path.join(BGFX_DIR, "src/renderer_d3d12.cpp"),
+    }
+
+    includedirs{
+        path.join(BX_DIR, "include"),
+        path.join(BIMG_DIR, "include"),
+        path.join(BGFX_DIR, "include"),
+        path.join(BGFX_DIR, "3rdparty"),
+        path.join(BGFX_DIR, "3rdparty/dxsdk/include"),
+        path.join(BGFX_DIR, "3rdparty/khronos"),
+    }
+
+    setBxCompact()
+
+project "bimg"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++14"
+    exceptionhandling "Off"
+    rtti "Off"
+
+    files{
+        path.join(BIMG_DIR, "include/bimg/*.h"),
+        path.join(BIMG_DIR, "src/image.cpp"),
+        path.join(BIMG_DIR, "src/image_gnf.cpp"),
+        path.join(BIMG_DIR, "src/*.h"),
+        path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc"),
+    }
+
+    includedirs{
+        path.join(BX_DIR, "include"),
+        path.join(BIMG_DIR, "include"),
+        path.join(BIMG_DIR, "3rdparty/astc-codec"),
+        path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
+    }
+    setBxCompact()
+
+project "bx"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++14"
+    exceptionhandling "Off"
+    rtti "Off"
+    defines "__STDC_FORMAT_MACROS"
+
+    files{
+        path.join(BX_DIR, "include/bx/*.h"),
+        path.join(BX_DIR, "include/bx/inline/*.inl"),
+        path.join(BX_DIR, "src/*.cpp"),
+    }
+
+    excludes{
+        path.join(BX_DIR, "src/amalgamated.cpp"),
+        path.join(BX_DIR, "src/crtnone.cpp"),
+    }
+
+    includedirs{
+        path.join(BX_DIR, "3rdparty"),
+        path.join(BX_DIR, "include"),
+    }
+    
+    setBxCompact()
